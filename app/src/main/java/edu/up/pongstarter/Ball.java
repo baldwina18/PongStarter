4ae60480e2093ade5d79981ce2a30f3c35883926
baldwina18 <baldwina18@up.edu>
2016-02-27 23:40:01 -0800

    part A completed

--- app/src/main/java/edu/up/pongstarter/Ball.java
+++ app/src/main/java/edu/up/pongstarter/Ball.java
@@ -1,11 +1,17 @@
 package edu.up.pongstarter;
 
 import android.graphics.Color;
-
 import java.util.ArrayList;
 
 /**
- * Created by alexabaldwin on 2/27/16.
+ * @author Alexa Baldwin
+ * @version 2/27/2016
+ */
+
+/**
+ * Ball class
+ * Each ball has an x-coordinate, y-coordinate, radius,
+ * x-speed, y-speed and color.
  */
 public class Ball {
     protected int color;
@@ -15,7 +21,9 @@ public class Ball {
     protected int xSpeed;
     protected int ySpeed;
 
-
+    /**
+     * Ball constructor - initializes instance variables
+     */
     public Ball () {
         xCoor = getRandWidth();
         yCoor = getRandHeight();
@@ -25,41 +33,71 @@ public class Ball {
         this.color = Color.WHITE;
     }
 
+    /**
+     * getRandHeight creates a random number based on the height of the canvas
+     * @return a random number for the height
+     */
     public int getRandHeight() {
-        int rand = (int)(Math.random()*(1294-50-rad)); //fix this so that it uses the canvas size
+        int rand = (int)(Math.random()*(1294-50-rad));
         return rand+(50+rad);
     }
 
+    /**
+     * getRandWidth creates a random number based on the width of the canvas
+     * @return a random number for the width
+     */
     public int getRandWidth() {
-        int rand = (int)(Math.random()*(2048-50-rad)); //fix this so that it uses the canvas size
+        int rand = (int)(Math.random()*(2048-50-rad));
         return rand+(50+rad);
     }
 
+    /**
+     * addBall adds a ball to the ArrayList of Balls from BallAnimator
+     * @param abl an ArrayList of Ball objects
+     */
     public void addBall(ArrayList<Ball> abl) {
         abl.add(this);
     }
 
-
+    /**
+     * reset ball - this method gets called when the ball exits the
+     * playing field and resets the ball's position and velocity randomly
+     */
     public void resetBall() {
-
         xCoor = getRandHeight();
         yCoor = getRandWidth();
         xCoor = (int)(Math.random()*60)+10;
         yCoor = (int)(Math.random()*60)+10;
     }
 
+    /**
+     * setxSpeed - resets the x-speed
+     * @param newSpeed new speed for the ball
+     */
     public void setxSpeed(int newSpeed) {
         xSpeed = newSpeed;
     }
 
+    /**
+     * setySpeed - resets the y-speed
+     * @param newSpeed new speed for the ball
+     */
     public void setySpeed(int newSpeed) {
         ySpeed = newSpeed;
     }
 
+    /**
+     * setxCoor - resets the x-coordinate
+     * @param newCoor new x-coor for the ball
+     */
     public void setxCoor(int newCoor) {
         xCoor = newCoor;
     }
 
+    /**
+     * setyCoor - resets the y-coordinate
+     * @param newCoor new y-coor for the ball
+     */
     public void setyCoor(int newCoor) {
         yCoor = newCoor;
     }
